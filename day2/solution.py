c95fa448ebaa17d12a11fa25e6681b20ad18897a
Luca Paolella <luca.paolella@gmail.com>
2022-12-02 09:40:32 +0100

    minor refactoring of day 2

--- day2/solution.py
+++ day2/solution.py
@@ -7,16 +7,17 @@ with open(file_path, "r") as f:
 opponent_options = "ABC"
 player_options = "XYZ"
 
+# convert all moves to the respective indices
 rounds = [[opponent_options.index(game.split(" ")[0]), player_options.index(game.split(" ")[1])] for game in data]
 
-def result(o_move_index, p_move_index):
-    return (2 - ((o_move_index - p_move_index + 1) % 3)) * 3
+def result(o, p):
+    return (2 - ((o - p + 1) % 3)) * 3
 
-def score(o_move_index, p_move_index):
-    return result(o_move_index, p_move_index) + p_move_index + 1
+def score(o, p):
+    return result(o, p) + p + 1
 
-def get_player_move(o_move_index, p_desired_result): # for p2
-    return (o_move_index + p_desired_result + 2) % 3
+def get_player_move(o, p_result): # for p2
+    return (o + p_result + 2) % 3
 
 def part1():
     return sum([score(o, p) for o, p in rounds])
