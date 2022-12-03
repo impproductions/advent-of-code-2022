24f923d60bbd2d2fdba155b9ceb79ba699769e7b
Luca Paolella <luca.paolella@gmail.com>
2022-12-03 15:20:20 +0100

    added ridiculous one-line version of day 2

--- day2/solution.py
+++ day2/solution.py
@@ -21,9 +21,15 @@ def get_player_move(o, p_result): # for p2
 
 def part1():
     return sum([score(o, p) for o, p in rounds])
+    # or, if you're a practical person and you enjoy clear, readable
+    # well documented code, here's the same thing in one line
+    # return sum([(2 - ((o - p + 1) % 3)) * 3 + p + 1 for o, p in rounds])
 
 def part2():
     return sum([score(o, get_player_move(o, p)) for o, p in rounds])
+    # or, if you're a practical person and you enjoy clear, readable
+    # well documented code, here's the same thing in one line
+    # return sum([(2 - ((o - (o + p + 2) % 3 + 1) % 3)) * 3 + (o + p + 2) % 3 + 1 for o, p in rounds])
 
 print(part1())
 print(part2())
