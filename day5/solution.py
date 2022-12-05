d6093da0514efe8fbc47dccfcbba9591fc2a4350
Luca Paolella <luca.paolella@gmail.com>
2022-12-05 09:24:05 +0100

    minor refactoring for day 5

--- day5/solution.py
+++ day5/solution.py
@@ -9,15 +9,19 @@ def parse_instructions():
     instructions = []
     for instruction in input[1].split("\n"):
         p1, p2 = instruction.split(" from ")
-        amt, frm, to = int(p1.split("move ")[1]), *[int(n) - 1 for n in p2.split(" to ")]
+        amt = int(p1.split("move ")[1])
+        frm, to = [int(n) - 1 for n in p2.split(" to ")]
         instructions.append((amt, frm, to))
 
     return instructions
 
+
 def parse_crate_configuration():
-    relevant_chars = [input[0][i + 1] for i in range(0, len(input[0]), 4)]
-    crate_count = int(relevant_chars[-1])
-    return [list(reversed([c for c in relevant_chars[i::crate_count][:-1] if c != " "])) for i in range(crate_count)]
+    crate_count = int(input[0][-2])
+    return [list(reversed([line[i * 4 + 1] for line in input[0].split("\n")
+                           if line[i * 4 + 1] != ' '][:-1]))
+            for i in range(crate_count)]
+
 
 initial_configuration = parse_crate_configuration()
 instructions = parse_instructions()
@@ -40,12 +44,12 @@ def do_procedure(instructions, crates, reverse_moving_stack):
 
 
 def part1():
-    crates = do_procedure(instructions, initial_configuration, reverse_moving_stack=True)
+    crates = do_procedure(instructions, initial_configuration, True)
     return "".join([crate[-1] for crate in crates])
 
 
 def part2():
-    crates = do_procedure(instructions, initial_configuration, reverse_moving_stack=False)
+    crates = do_procedure(instructions, initial_configuration, False)
     return "".join([crate[-1] for crate in crates])
 
 
