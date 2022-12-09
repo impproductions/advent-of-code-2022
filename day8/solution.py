eb87017bb99caeac9cb2276d7c904339e4020ef0
Luca Paolella <luca.paolella@gmail.com>
2022-12-09 14:43:24 +0100

    switched day 8 grid to dict, solution for day 9

--- day8/solution.py
+++ day8/solution.py
@@ -6,19 +6,21 @@ path = Path(__file__).parent / "input.txt"
 with path.open() as file:
     input = file.read()
 
-grid = [[int(char) for char in line] for line in input.splitlines()]
-height, width = len(grid), len(grid[0])
-x_bounds, y_bounds = range(1, len(grid[0])-1), range(1, len(grid)-1)
+data = [[int(char) for char in line] for line in input.splitlines()]
+height, width = len(data), len(data[0])
+grid = {(x, y): data[y][x]
+        for x in range(width) for y in range(height)}
+x_bounds, y_bounds = range(1, width-1), range(1, height-1)
 
 
 def look_around(x, y):
     visible = 0
     l, r, t, b = 0, 0, 0, 0
-    tree_height = grid[y][x]
+    tree_height = grid[(x, y)]
 
     # look left
     for xl in range(x-1, -1, -1):
-        watched_height = grid[y][xl]
+        watched_height = grid[(xl, y)]
         l = x - xl
         if tree_height <= watched_height:
             break
@@ -26,17 +28,17 @@ def look_around(x, y):
             visible = 1
 
     # look right
-    for xr in range(x+1, len(grid[y])):
-        watched_height = grid[y][xr]
+    for xr in range(x+1, len(data[y])):
+        watched_height = grid[(xr, y)]
         r = xr - x
         if tree_height <= watched_height:
             break
-        if r == len(grid[y]) - (x+1):
+        if r == len(data[y]) - (x+1):
             visible = 1
 
     # look up
     for yt in range(y-1, -1, -1):
-        watched_height = grid[yt][x]
+        watched_height = grid[(x, yt)]
         t = y - yt
         if tree_height <= watched_height:
             break
@@ -44,12 +46,12 @@ def look_around(x, y):
             visible = 1
 
     # look down
-    for yb in range(y+1, len(grid)):
-        watched_height = grid[yb][x]
+    for yb in range(y+1, len(data)):
+        watched_height = grid[(x, yb)]
         b = yb - y
         if tree_height <= watched_height:
             break
-        if b == len(grid) - (y+1):
+        if b == len(data) - (y+1):
             visible = 1
 
     return visible, l * r * t * b
---
+++ day9/solution.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+from math import ceil, floor
+from pprint import pprint
+
+path = Path(__file__).parent / "input.txt"
+with path.open() as file:
+    input = file.read().splitlines()
+
+moves = [tuple(map(int, line.replace("R ", "0,+")
+                   .replace("L ", "0,-")
+                   .replace("U ", "1,+")
+                   .replace("D ", "1,-")
+                   .split(","))) for line in input]
+
+
+def sign(n):
+    if n == 0:
+        return 0
+    if n < 0:
+        return -1
+    if n > 0:
+        return 1
+
+
+def cap(n):
+    return sign(n) * min(abs(n), 1)
+
+
+def follow(H, T):
+    new_pos = T
+    hx, hy = H
+    tx, ty = T
+    dHT = (hx-tx, hy-ty)
+    dx, dy = dHT
+
+    if abs(dx) == 2 or abs(dy) == 2:
+        new_pos = (tx + cap(dx), ty + cap(dy))
+
+    return tuple(new_pos)
+
+
+def simulate_rope(length):
+    visited = set()
+    rope = [[0,0]] + [(0, 0) for i in range(1, length)]
+
+    for move in moves:
+        axis, amt = move
+
+        # execute the movement 1 step at a time
+        for _ in range(abs(amt)):
+            # move the head
+            rope[0][axis] += sign(amt)
+
+            # for every element of the rope, up to the tail
+            for pos in range(1, len(rope)):
+                rope[pos] = follow(rope[pos-1], rope[pos])
+
+            # track the last element of the rope
+            visited.add(rope[length-1])
+
+    return len(visited)
+
+
+def part1():
+    return simulate_rope(2)
+
+
+def part2():
+    return simulate_rope(10)
+
+
+print(part1())
+print(part2())
