522a75dc9b8402bfd4f31ee98b27db885e563a78
Luca Paolella <luca.paolella@gmail.com>
2022-12-14 18:14:18 +0100

    minor refactoring for day 14

--- day14/solution.py
+++ day14/solution.py
@@ -1,40 +1,29 @@
 from pathlib import Path
 
 
-# def print_lines(points: dict[tuple, str]):
-#     min_x, max_x = min([p[0] for p in occupied]), max([p[0] for p in occupied])
-#     min_y, max_y = min([p[1] for p in occupied]), max([p[1] for p in occupied])
-#     for y in range(min_y, max_y+1):
-#         line = ""
-#         for x in range(min_x, max_x+1):
-#             line += points.get((x, y), ".")
-#         print(line)
-
-
 path = Path(__file__).parent / "input.txt"
 with path.open() as file:
     input = file.read().splitlines()
 
-occupied = {(500, 0): "+"}
+start = (500, 0)
+occupied = {start: "+"}
 
 for path in input:
     split_path = path.split(" -> ")
     for i, point in enumerate(split_path):
-        if i < 1:
-            continue
-        px, py = [int(c) for c in split_path[i-1].split(",")]
-        cx, cy = [int(c) for c in split_path[i].split(",")]
-        for i in range(*sorted((py, cy+1))):
-            occupied[(cx, i)] = "#"
-        for i in range(*sorted((px, cx+1))):
-            occupied[(i, cy)] = "#"
+        if i >= 1:
+            px, py = [int(c) for c in split_path[i-1].split(",")]
+            cx, cy = [int(c) for c in split_path[i].split(",")]
+            for i in range(*sorted((py, cy+1))):
+                occupied[(cx, i)] = "#"
+            for i in range(*sorted((px, cx+1))):
+                occupied[(i, cy)] = "#"
 
 min_x, max_x = min([p[0] for p in occupied]), max([p[0] for p in occupied])
 max_y = max([p[1] for p in occupied])
 
 
-def check_spot(pos, has_floor):
-    next = pos
+def check_spot(next, has_floor):
     if not has_floor and not next[0] in range(min_x, max_x+1):
         return "void"
     if occupied.get(next) == None:
@@ -42,57 +31,49 @@ def check_spot(pos, has_floor):
             return "rest"
         return next
 
-    return "empty"
+    return "wall"
 
 
 def get_next_free(pos, has_floor):
     next = check_spot((pos[0], pos[1] + 1), has_floor)
-    if next != "empty":
+    if next != "wall":
         return next
 
     next = check_spot((pos[0]-1, pos[1] + 1), has_floor)
-    if next != "empty":
+    if next != "wall":
         return next
 
     next = check_spot((pos[0]+1, pos[1] + 1), has_floor)
-    if next != "empty":
+    if next != "wall":
         return next
 
     return "rest"
 
 
-def drop_grain(has_floor):
-    current_pos = (500, 0)
-    while current_pos != "rest":
-        next = get_next_free(current_pos, has_floor)
-        if next == "void":
-            return False
-        if next == "rest":
-            if occupied.get(current_pos, None) == "+":
-                occupied[current_pos] = "o"
-                return False
-            occupied[current_pos] = "o"
-        current_pos = next
-
-    return True
-
-
 def drop_sand(has_floor):
-    can_continue = True
-    while can_continue:
-        can_continue = drop_grain(has_floor)
+    while True:
+        current_pos = start
+        while current_pos != "rest":
+            next = get_next_free(current_pos, has_floor)
+            if next == "void":
+                return
+            elif next == "rest":
+                if occupied.get(current_pos, None) == "+":
+                    return
+                occupied[current_pos] = "o"
+            current_pos = next
 
 
 def part1():
     drop_sand(False)
 
-    return sum([1 for cell in occupied.values() if cell == "o"])
+    return sum(1 for cell in occupied.values() if cell == "o")
 
 
 def part2():
     drop_sand(True)
 
-    return sum([1 for cell in occupied.values() if cell == "o"])
+    return sum(1 for cell in occupied.values() if cell == "o" or cell == "+")
 
 
 print(part1())
